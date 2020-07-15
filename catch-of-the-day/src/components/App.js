baf48b1292ffdab1b8019891ba497ce6907fd99a
Joshua Tuscan <hello@joshuatuscan.com>
2020-07-15 14:09:49 -0400

    did our app routing and adding a fish to state

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,14 +4,29 @@ import Inventory from "./Inventory";
 import Order from "./Order";
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {},
+  };
+
+  addFish = (fish) => {
+    // console.log(fish);
+    // 1. take a copy of the existing state
+    const fishes = { ...this.state.fishes };
+    // 2. add our new fish to that fishes variable
+    fishes[`fish${Date.now()}`] = fish;
+    // 3. set the new fishes object into the state
+    this.setState({ fishes });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
-          <Header />
+          <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
     );
   }
